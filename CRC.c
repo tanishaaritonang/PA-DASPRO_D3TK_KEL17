94ef5fde72d9deb857bc748f54b94ace8b34ec3b
13323046_TANISHA NATALIA ARITONANG <148616765+tanishaaritonang@users.noreply.github.com>
2023-12-02 11:30:54 +0700

    hapus
    menghapus beberapa

--- CRC.c
+++ CRC.c
@@ -1,36 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-// Fungsi untuk mengkonversi NIM menjadi biner
 void convertToBinary(char nim[], char binary[]) {
-    // Implementasi konversi dari NIM ke biner di sini
-    // ...
-
-    // Contoh implementasi sederhana (asumsi NIM adalah bilangan)
-    int decimalNIM = atoi(nim);
-    itoa(decimalNIM, binary, 2);
+    int n = atoi(nim);
+    int size = sizeof(n) * 8;
+    int index = 0;
+    for (int i = size - 1; i >= 0; i--) {
+        if ((n >> i) & 1)
+            binary[index++] = '1';
+        else
+            binary[index++] = '0';
+    }
+    binary[index] = '\0';
 }
 
-// Fungsi untuk menghitung CRC
 void calculateCRC(char data[], char divisor[], char crcResult[]) {
     // Implementasi perhitungan CRC di sini
     // ...
 
-    // Contoh implementasi sederhana (asumsi XOR sebagai operasi pembagian)
-    strcpy(crcResult, data);
-    // Operasi XOR antara data dan divisor
-    for (int i = 0; i < strlen(data) - strlen(divisor) + 1; i++) {
-        if (crcResult[i] == '1') {
-            for (int j = 0; j < strlen(divisor); j++) {
-                crcResult[i + j] = crcResult[i + j] == divisor[j] ? '0' : '1';
+    // Kode perhitungan CRC yang sesuai
+    // ...
+
+    strcpy(crcResult, "0000"); // Contoh nilai CRC kosong untuk sekarang
+}
+
+void binerKePolinomial(int bilangan, FILE *outputFile) {
+    if (bilangan == 0) {
+        fprintf(outputFile, "CRC: 0\n");
+        printf("Polinomial: 0\n");
+        return;
+    }
+
+    int panjang = log2(bilangan) + 1;
+    fprintf(outputFile, "CRC: ");
+    printf("Polinomial: ");
+    for (int i = panjang - 1; i >= 0; i--) {
+        if ((bilangan >> i) & 1) {
+            if (i == 1) {
+                fprintf(outputFile, " + x");
+                printf(" + x");
+            } else if (i == 0) {
+                fprintf(outputFile, " + x^0");
+                printf(" + x^0");
+            } else {
+                if (i == panjang - 1) {
+                    fprintf(outputFile, "x^%d", i);
+                    printf("x^%d", i);
+                } else {
+                    fprintf(outputFile, " + x^%d", i);
+                    printf(" + x^%d ", i);
+                }
             }
         }
     }
+    fprintf(outputFile, "\n");
+    printf("\n");
 }
 
 int main() {
-    // Input data NIM
     char nim[20][10];
     printf("Input 20 NIM:\n");
     for (int i = 0; i < 20; i++) {
@@ -38,29 +67,28 @@ int main() {
         scanf("%s", nim[i]);
     }
 
-    // Output program dalam .txt
     FILE *outputFile;
     outputFile = fopen("output.txt", "w");
 
     for (int i = 0; i < 20; i++) {
-        char binary[64];
+        char binary[160];
         char crcResult[64];
 
-        // 1. Konversi NIM menjadi biner
         convertToBinary(nim[i], binary);
         fprintf(outputFile, "a. NIM %d: %s\n", i + 1, nim[i]);
         fprintf(outputFile, "b. Biner: %s\n", binary);
 
-        // 2. Hitung CRC
-        char divisor[64] = "10011";  // Contoh primitive polynomial
+        char divisor[64] = "10011";
         calculateCRC(binary, divisor, crcResult);
 
-        // 3. Output hasil
         fprintf(outputFile, "c. Hasil CRC: %s\n", crcResult);
         fprintf(outputFile, "d. Primitive Polynomial: %s\n", divisor);
         fprintf(outputFile, "e. Primitive Polynomial (Biner): %s\n", divisor);
         fprintf(outputFile, "f. Hasil Transmisi CRC: %s\n", crcResult);
         fprintf(outputFile, "g. Pembuktian CRC: %s\n\n", strcmp(crcResult, "0000") == 0 ? "Valid" : "Tidak Valid");
+
+        int bilangan = atoi(crcResult);
+        binerKePolinomial(bilangan, outputFile);
     }
 
     fclose(outputFile);
